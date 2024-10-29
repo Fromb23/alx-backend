4ef9a0a798cb7375d249120e65ac4fd9ecfe3341
Francis Rombo <fromb@kali.>
2024-10-29 17:58:17 +0000

    baseCaching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""caching system """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Basic Cache inherits from BaseCaching and is a caching system """
+    base_caching = BaseCaching()
+
+    def put(self, key, item):
+        """Add key-value pair to the cache
+        using self.cache_data from BaseCaching."""
+        if key is None and item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve value from cache based on the key."""
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
