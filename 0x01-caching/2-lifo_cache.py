37523433abc3544b5df454bb7af5f3e63b7c9b9c
Francis Rombo <fromb@kali.>
2024-10-29 19:28:57 +0000

    baseCaching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -17,7 +17,7 @@ class LIFOCache(BaseCaching):
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             last_key = self.order.pop()
             del self.cache_data[last_key]
-            print("DISCARD: {last_key}")
+            print(f"DISCARD: {last_key}")
 
         self.cache_data[key] = item
         self.order.append(key)
